b7e5e970c8cd74245aa9fb5591252ef9cc670764
Justine Cantado <justinecantado@gmail.com>
2017-08-13 15:15:57 +0000

    Login finished

--- scripts/hide.js
+++ scripts/hide.js
@@ -3,8 +3,8 @@
 window.onload = function(){ 
 document.getElementById("continue").onclick = function() {
     var $banner = document.getElementById('banner');
-    $banner.style.marginLeft = "-445px";
-    $banner.style.opacity = '0';
+    $banner.style.marginLeft = "-330px";
+    $banner.style.opacity = '0.1';
 };
 
 };
